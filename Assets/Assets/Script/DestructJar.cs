6a2ecb967131a9f6b7b347127bc7da2527e890d2
akira894 <kvnhela@gmail.com>
2015-11-23 10:39:40 +0100

    InitialVersion

---
+++ Assets/Assets/Script/DestructJar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructJar : MonoBehaviour {
+	public GameObject ObjetDrop;
+	public Vector3 TransformP; 
+	public int NbObjetPop = 1;
+	// Use this for initialization
+	void Start () {
+		TransformP	= transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other) // Attention RigidBody et BoxCollider2d pour les deux
+	{
+		Debug.Log ("Je suis passé");
+		if (other.gameObject.name == "PlayerTest") {
+			Destroy(gameObject);
+		}
+		if (NbObjetPop > 0) 
+		{
+			Instantiate (ObjetDrop, TransformP, this.transform.rotation);
+			// Donner une animation à la l'objet drop( de haut en bas, pour tous les objets --> facile à gérer )
+			NbObjetPop -= 1;
+		}
+
+	}
+}
---
+++ Assets/Assets/Script/MovePlayerTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayerTest : MonoBehaviour {
+
+	public float speed = 0.2f; // vitesse de déplacement
+	private Vector3 NewPosition; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		NewPosition.x = Mathf.Clamp(Input.GetAxis ("Horizontal") * speed + this.gameObject.transform.position.x,-8.03f,+8.03f);
+		NewPosition.y = Mathf.Clamp(Input.GetAxis ("Vertical") * speed + this.gameObject.transform.position.y,- 5.0f,5.0f);
+		NewPosition.z = this.gameObject.transform.position.z;
+		
+		this.gameObject.transform.position = NewPosition; 
+
+	}
+}
